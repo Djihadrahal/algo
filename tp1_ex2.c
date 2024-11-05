c97529b1cf2c9a9067f3811342de6c0fee411843
Djihadrahal <jwjwa9262@gmail.com>
2024-11-05 08:19:39 +0100

    Update tp1_ex2.c

--- tp1_ex2.c
+++ tp1_ex2.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+void transpose(int matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+void printMatrix(int matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[N][N] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    printf("Original Matrix:\n");
+    printMatrix(matrix);
+
+    transpose(matrix);
+
+    printf("\nTransposed Matrix:\n");
+    printMatrix(matrix);
+
+    return 0;
+}
